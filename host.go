ea6539221acadf9f8bc6866e5911d7efc7656da9
Christopher Wellons <wellons@nullprogram.com>
2020-02-07 14:49:41 -0500

    Initial working testbed

---
+++ host.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+type logger struct {
+	h http.Handler
+}
+
+func (l *logger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.Method, r.URL)
+	l.h.ServeHTTP(w, r)
+}
+
+func main() {
+	port := flag.Int("p", 8001, "HTTP port number")
+	flag.Parse()
+
+	args := []string{
+		"daemon",
+		"--base-path=./src",
+		"--listen=127.0.0.1",
+		"--reuseaddr",
+		"--export-all",
+	}
+	cmd := exec.Command("git", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	go func() {
+		log.Println("git", strings.Join(args, " "))
+		err := cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	fs := http.FileServer(http.Dir("www"))
+	http.Handle("/", &logger{fs})
+	addr := "127.0.0.1:" + strconv.Itoa(*port)
+	log.Println("listening at", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
